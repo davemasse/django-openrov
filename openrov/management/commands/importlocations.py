6b099236e6c0e5dc2edc09cee7dcbb7adf2a1ea4
Dave Masse <dmasse@rudemoose.com>
2012-07-12 18:07:44 -0400

    Add dependencies to setup.py. Use requests module in place or urllib2. Clean up calls to Catch API.

--- openrov/management/commands/importlocations.py
+++ openrov/management/commands/importlocations.py
@@ -1,9 +1,7 @@
-import base64
 import datetime
 import re
-import simplejson
+import requests
 import time
-import urllib2
 from optparse import make_option
 
 from django.conf import settings
@@ -11,6 +9,10 @@ from django.core.management.base import BaseCommand
 from django.utils import timezone
 
 from openrov.models import Location, Video
+
+PAGE_SIZE = 10
+TAG_REGEX = re.compile(r' *#[^ ]+')
+VIDEO_REGEX = re.compile(r'(?P<video_url>http://(?:www\.)?youtube.com/watch\?\S*?v=(?P<video_id>[^&\s]+))')
 
 class Command(BaseCommand):
   help = 'Collects remote text via Catch API'
@@ -23,54 +25,64 @@ class Command(BaseCommand):
     )
   
   def handle(self, *args, **options):
-    request = urllib2.Request('https://api.catch.com/v2/search.json?q=#openrov&sort=modified_asc&limit=1000')
-    base64string = base64.encodestring('%s:%s' % (settings.CATCH_USERNAME, settings.CATCH_PASSWORD)).replace('\n', '')
-    request.add_header("Authorization", "Basic %s" % base64string)   
-    result = urllib2.urlopen(request)
-    data = simplejson.loads(result.read())
+    offset = 0
     
-    notes = data.get('notes', [])
-    remote_ids = []
+    # Get into the loop
+    count = 10
+    page_num = 0
     
-    for note in notes:
-      remote_id = note['id']
-      remote_ids.append(remote_id)
-      remote_date_modified = note['server_modified_at']
-      remote_date_modified = datetime.datetime.strptime(remote_date_modified, '%Y-%m-%dT%H:%M:%S.%fZ')
-      remote_date_modified = remote_date_modified.replace(tzinfo=timezone.utc)
+    while count == 10:
+      params = {
+        'full': True,
+        'limit': PAGE_SIZE,
+        'offset': PAGE_SIZE * page_num,
+        'q': '#openrov',
+        'sort': 'modified_desc',
+      }
+      r = requests.get('https://api.catch.com/v2/search.json', params=params, auth=(settings.CATCH_USERNAME, settings.CATCH_PASSWORD))
+      data = r.json
       
-      try:
-        location = Location.objects.get(remote_id=remote_id)
+      count = data['count']
+      remote_ids = []
+      
+      for note in data.get('notes', []):
+        remote_id = note['id']
+        remote_ids.append(remote_id)
+        remote_date_modified = note['server_modified_at']
+        remote_date_modified = datetime.datetime.strptime(remote_date_modified, '%Y-%m-%dT%H:%M:%S.%fZ')
+        remote_date_modified = remote_date_modified.replace(tzinfo=timezone.utc)
         
-        # Don't process pre-existing locations that are already up-to-date
-        if location.remote_date_modified >= remote_date_modified:
-          continue
-      except Location.DoesNotExist:
-        location = Location(remote_id=remote_id, remote_date_modified=remote_date_modified)
+        try:
+          location = Location.objects.get(remote_id=remote_id)
+          # Don't process pre-existing locations that are already up-to-date
+          if location.remote_date_modified >= remote_date_modified:
+            print '[N] %s' % (remote_id,)
+            continue
+          else:
+            print '[U] %s' % (remote_id,)
+        except Location.DoesNotExist:
+          location = Location(remote_id=remote_id, remote_date_modified=remote_date_modified)
+          print '[C] %s' % (remote_id,)
+        
+        location.remote_date_created = note['server_created_at']
+        location.remote_date_modified = note['server_modified_at']
+        location.lat = note['location']['features'][0]['geometry']['coordinates'][1]
+        location.lng = note['location']['features'][0]['geometry']['coordinates'][0]
+        location.save()
+        
+        remote_text = note['text'].strip()
+        for matches in VIDEO_REGEX.finditer(remote_text):
+          Video.objects.get_or_create(location=location, video_id=matches.group('video_id'))
+          remote_text = remote_text.replace(' %s' % matches.group('video_url'), '')
+        
+        location.remote_text = remote_text
+        location.description = location.description = TAG_REGEX.sub('', remote_text)
+        location.save()
       
-      request = urllib2.Request('https://api.catch.com/v2/notes/%s.json' % remote_id)
-      request.add_header("Authorization", "Basic %s" % base64string)   
-      result = urllib2.urlopen(request)
-      data = simplejson.loads(result.read())
-      
-      remote_note = data['notes'][0]
-      remote_date_created = remote_note['server_created_at']
-      remote_text = remote_note['text'].strip()
-      lat = remote_note['location']['features'][0]['geometry']['coordinates'][1]
-      lng = remote_note['location']['features'][0]['geometry']['coordinates'][0]
-      
-      location.remote_date_created = remote_date_created
-      location.lat = lat
-      location.lng = lng
-      location.save()
-      
-      for matches in re.finditer(r'(?P<video_url>http://(?:www\.)?youtube.com/watch\?\S*?v=(?P<video_id>[^&\s]+))', remote_text):
-        Video.objects.get_or_create(location=location, video_id=matches.group('video_id'))
-        remote_text = remote_text.replace(' %s' % matches.group('video_url'), '')
-      location.remote_text = remote_text
-      location.description = location.description = re.sub(r' *#[^ ]+', '', remote_text)
-      location.save()
+      page_num += 1
     
     # Delete Location objects that don't exist remotely
     if options.get('delete') == True:
-      Location.objects.exclude(remote_id__in=remote_ids).delete()
+      to_delete = Location.objects.exclude(remote_id__in=remote_ids)
+      'Deleting %s oudated Location objects' % (to_delete.count(),)
+      to_delete.delete()
--- setup.py
+++ setup.py
@@ -3,5 +3,5 @@ from distutils.core import setup
 setup(name='openrov',
   version='0.1',
   url='https://github.com/davemasse/django-openrov',
-  packages=['openrov'],
+  packages=['django', 'openrov', 'requests', 'south'],
 )
